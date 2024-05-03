6b2e3f94d1086f3379a0331564a1d575ee4ed54d
Goh Matsumoto <urimaro@gmail.com>
2024-05-03 23:02:35 +0900

    Fix 'AttributeError: 'WasRun' object has no attribute 'wasSetUp'' and call `setUp` before test case executions: P153

--- xunit.py
+++ xunit.py
@@ -2,7 +2,11 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
@@ -10,6 +14,9 @@ class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
         super().__init__(name)
+
+    def setUp(self):
+        self.wasSetUp = 1
 
     def testMethod(self):
         self.wasRun = 1
